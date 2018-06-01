92da0dec5d47e4f25fc08af8c83252b83c13bfbb
Carlos Arroyo Villalobos <carlosarrovi96@gmail.com>
2018-06-01 12:05:53 -0600

    Urls changes and models delete

--- simpleblockchain/urls.py
+++ simpleblockchain/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 from rest_framework.urlpatterns import format_suffix_patterns
 from simpleblockchain import views
 
 urlpatterns = [
-    path('mine', views.MinningView.as_view()),
+    path('', RedirectView.as_view(pattern_name='chain', permanent=False)),
+    path('mine', views.MinningView.as_view(), name='chain'),
     path('transactions/new', views.TransactionView.as_view()),
     path('chain', views.FullChainView.as_view()),
     path('nodes/register', views.RegisterNodesView.as_view()),
